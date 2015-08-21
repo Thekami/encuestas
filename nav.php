5cab5c03ebd675131f9adc50c1cef6dd8b44e671
Angel Torres <thekamitorres@gmail.com>
2015-08-21 13:11:18 -0500

    navbar

---
+++ nav.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Navbar</title>
+</head>
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/skeleton.css">
+  <link href='//fonts.gstatic.com/s/raleway/v9/xkvoNo9fC8O2RDydKj12bwzyDMXhdD8sAj6OAJTFsBI.woff2' rel='stylesheet' type='text/css'>
+  <style>
+	
+	@font-face {
+		font-family: "Raleway";
+		font-style: normal;
+		font-weight: 600;
+		src: local("Raleway SemiBold"), local("Raleway-SemiBold"), url("http://fonts.gstatic.com/s/raleway/v9/xkvoNo9fC8O2RDydKj12bwzyDMXhdD8sAj6OAJTFsBI.woff2") format("woff2"), url("http://fonts.gstatic.com/s/raleway/v9/xkvoNo9fC8O2RDydKj12b73hpw3pgy2gAi-Ip7WPMi0.woff") format("woff");
+	}
+
+	nav.primary{
+	display: table;
+	margin: 0 auto;
+	width: 100%;
+	/*background-color: red;*/
+	border: solid 1px gray;
+	 }
+	nav.primary ul,
+	nav.primary ul li {
+	margin: 0px;
+	}
+
+	nav.primary select {
+	display: none;
+	width:  100%;
+	height: 28px;
+	margin: 21px 0;
+	}
+
+	nav.primary ul li {
+	display: inline;
+	float: left;
+	position: relative;}
+
+	nav.primary ul li a {
+	display: inline-block;
+	line-height: 49px;
+	padding:  0 14px;
+	color: black;
+	text-transform: uppercase;
+	text-decoration: none;
+	font-weight: Raleway;
+	letter-spacing: 0.08em;
+	background: ##999999;
+
+	}
+
+	nav.primary ul li a:hover {
+	background: #d9e7e6;
+	cursor: pointer;
+
+	}
+  </style>
+<body>
+	<!--Navigation-->
+<div class="band navigation">
+    <nav class=" primary">
+        <div class="sixteen columns">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Projects</a></li>
+                <li><a href="#">Contact Us</a></li>
+            </ul>
+        </div>
+    </nav>
+</div>
+
+<div class="container">
+	
+	<p>sdcs</p>
+	<br><br>
+	<p>sdcs</p>
+	<br><br>
+	<p>sdcs</p>
+	<br><br>
+	<p>sdcs</p>
+	<br><br>
+	<p>sdcs</p>
+	<br><br>
+	<p>sdcs</p>
+	<br><br>
+	<p>sdcs</p>
+	<br><br>
+	<p>sdcs</p>
+	<br><br>
+	<p>sdcs</p>
+	<br><br>
+	<p>sdcs</p>
+	<br><br>
+	<p>sdcs</p>
+	<br><br>
+	<p>sdcs</p>
+	<br><br>
+	<p>sdcs</p>
+	<br><br>
+	<p>sdcs</p>
+	<br><br>
+	<p>sdcs</p>
+	<br><br>
+	<p>sdcs</p>
+	<br><br>
+	<p>sdcs</p>
+	<br><br>
+	<p>sdcs</p>
+	<br><br>
+	<p>sdcs</p>
+	<br><br>
+	<p>sdcs</p>
+	<br><br>
+	<p>sdcs</p>
+	<br><br>
+	<p>sdcs</p>
+	<br><br>
+	<p>sdcs</p>
+	<br><br>
+	<p>sdcs</p>
+	<br><br>
+	<p>sdcs</p>
+	<br><br>
+	<p>sdcs</p>
+	<br><br> 
+
+</div>
+
+
+
+	
+</body>
+</html>
--- panel.php
+++ panel.php
@@ -18,68 +18,70 @@
   <link rel="stylesheet" href="css/normalize.css">
   <link rel="stylesheet" href="css/skeleton.css">
 <body>
-	
-	<div id="div_main" style="border: solid 1px; padding: 1% 3%; margin: 1% 0% 2% 0%">
-		<!-- <h2>Hola <?php echo $_SESSION["username"]; ?>!</h2> -->
-		<a href="logout.php">Cerrar Sesion</a>
-	</div>
-	
-	<div id="div_main" style="border: solid 1px; padding: 1% 3%; margin: 1% 0% 2% 0%">
-		<form action="">
-			<label for=""> Crear nueva encuesta</label>
-			<input type="submit" value="Crear" id="make_poll">
-			<label for=""> Editar encuesta</label>
-			<input type="submit" value="Editar" id="edit_poll">
-		</form>
-
-	</div>
-
-	<div id="div_edit" style="border: solid 1px; padding: 1% 3%; margin: 1% 0% 2% 0%">
+	<div class="container">
+		<div id="div_main" style="border: solid 1px; padding: 1% 3%; margin: 1% 0% 2% 0%">
+			<!-- <h2>Hola <?php echo $_SESSION["username"]; ?>!</h2> -->
+			<a href="logout.php">Cerrar Sesion</a>
+		</div>
 		
-		<select name="" id="select_poll"></select>
-
-		<select name="" id="select_quest">
-			<option value="0" selected disabled>Selecione una encuesta</option>
-		</select>
-
-	</div>
-	
-	<div id="div_create" style="border: solid 1px; padding: 1% 3%; margin: 1% 0% 2% 0%">
+			
 		
+		<div id="div_main" class="row" style="border: solid 1px; padding: 1% 3%; margin: 1% 0% 2% 0%">
+			<form action="">
+				<label for=""> Crear nueva encuesta</label>
+				<input type="submit" value="Crear" id="make_poll">
+				<label for=""> Editar encuesta</label>
+				<input type="submit" value="Editar" id="edit_poll">
+			</form>
 
-	</div>
+		</div>
 
-	<div id="div_show_edit" style="border: solid 1px; padding: 1% 3%; margin: 1% 0% 2% 0%">
+		<div id="div_edit" style="border: solid 1px; padding: 1% 3%; margin: 1% 0% 2% 0%">
+			
+			<select name="" id="select_poll"></select>
+
+			<select name="" id="select_quest">
+				<option value="0" selected disabled>Selecione una encuesta</option>
+			</select>
+
+		</div>
 		
+		<div id="div_create" style="border: solid 1px; padding: 1% 3%; margin: 1% 0% 2% 0%">
+			
 
+		</div>
+
+		<div id="div_show_edit" style="border: solid 1px; padding: 1% 3%; margin: 1% 0% 2% 0%">
+			
+
+		</div>
+
+
+		<div id="div_btn_new_quest" style="border: solid 1px; padding: 1% 3%; margin: 1% 0% 2% 0%">
+			
+			<form action="">
+				<label for=""> Nueva pregunta</label>
+				<input type="submit" value="Crear" id="new_quest">
+			</form>
+
+
+		</div>
+
+		<div id="div_btn_send_poll" style="border: solid 1px; padding: 1% 3%; margin: 1% 0% 2% 0%">
+			
+			<form action="">
+				<label for=""> Enviar</label>
+				<input type="submit" value="Enviar" id="send_poll">
+			</form>
+
+
+		</div>
+
+		<div id="div_show_url" style="border: solid 1px; padding: 1% 3%; margin: 1% 0% 2% 0%">
+			
+
+		</div>
 	</div>
-
-
-	<div id="div_btn_new_quest" style="border: solid 1px; padding: 1% 3%; margin: 1% 0% 2% 0%">
-		
-		<form action="">
-			<label for=""> Nueva pregunta</label>
-			<input type="submit" value="Crear" id="new_quest">
-		</form>
-
-
-	</div>
-
-	<div id="div_btn_send_poll" style="border: solid 1px; padding: 1% 3%; margin: 1% 0% 2% 0%">
-		
-		<form action="">
-			<label for=""> Enviar</label>
-			<input type="submit" value="Enviar" id="send_poll">
-		</form>
-
-
-	</div>
-
-	<div id="div_show_url" style="border: solid 1px; padding: 1% 3%; margin: 1% 0% 2% 0%">
-		
-
-	</div>
-	
 	<script src="js/jquery-1.11.2.min.js"></script>
 	<script src="js/panel.js"></script>
 </body>
