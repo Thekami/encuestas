134719af21dc5b3a6a78332810cab1d61da0544d
Angel Torres <thekamitorres@gmail.com>
2015-08-21 11:32:51 -0500

    index y landing con estilo

--- index.php
+++ index.php
@@ -18,11 +18,17 @@
 			
 			<div>
 
-				<form action="">
-					<h3>Inicio de Sesion</h3>
-					<input type="text" placeholder="Usuario" id="txt_user">
-					<input type="password" placeholder="Contraseña" id="txt_pass">
-					<input type="submit" value="Enviar" id="btn_login">
+				<form class="form-login" action="">
+					<div class="form-login-head">
+						<h3>Inicio de Sesion</h3>
+					</div>
+
+					<div class="form-login-body">
+						<input type="text" placeholder="Usuario" id="txt_user">
+						<input type="password" placeholder="Contraseña" id="txt_pass">
+						<input type="submit" value="Enviar" class="button-primary" id="btn_login">
+					</div>
+					
 					<p id="msgText"></p>
 				</form>
 
--- landing.php
+++ landing.php
@@ -1,13 +1,46 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<title>¿Estas perdido?</title>
-</head>
+
+  <!-- Basic Page Needs
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta charset="utf-8">
+  <title>¿Estas perdido?</title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <!-- Mobile Specific Metas
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- FONT
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
+
+  <!-- CSS
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
   <link rel="stylesheet" href="css/normalize.css">
   <link rel="stylesheet" href="css/skeleton.css">
+
+  <!-- Favicon
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="icon" type="image/png" href="images/favicon.png">
+
+</head>
 <body>
-	<h2>Lo sentimos pero esta pagina no esta disponible a menos que inicies sesion primero</h2>
-	<a href="index.php">Inicio</a>
+
+  <!-- Primary Page Layout
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <div class="container">
+    <div class="row">
+      <div class="one-half column" style="margin-top: 25%">
+        <h2>Lo sentimos</h2>
+        <p style="font-size: 1.2em">Esta pagina no esta disponible a menos que inicies sesion primero <a href="index.php">Inicio</a>.</p>
+      </div>
+    </div>
+  </div>
+
+<!-- End Document
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
 </body>
 </html>
