d23e07eb4715350ec67233df3420b4fecade3572
Angel Torres <thekamitorres@gmail.com>
2015-08-22 15:48:27 -0500

    solo documentación y limpieza de panel.php

--- panel.php
+++ panel.php
@@ -20,15 +20,7 @@
   <link href='//fonts.gstatic.com/s/raleway/v9/xkvoNo9fC8O2RDydKj12bwzyDMXhdD8sAj6OAJTFsBI.woff2' rel='stylesheet' type='text/css'>
 <body>
 
-<!-- 	<nav class="primary">
-		<div class="container">
-		    <ul>
-		        <li><a style="color: #222" class="navbar-link" id="make_poll" href="#">Nueva encuesta</a></li>
-		        <li><a style="color: #222" class="navbar-link" id="edit_poll" href="#">Editar encuesta</a></li>
-		        <li><a style="color: #222" class="navbar-link" href="logout.php">Cerrar Sesion</a></li>
-		    </ul>
-		</div>
-	</nav> -->
+
 	<div class="navbar-container">
 		<nav class="primary">
 			<div class="container">
@@ -44,6 +36,8 @@
 
 	<div class="container">
 
+		<!-- ============ Seccion para editar  ================= -->
+
 		<div id="div_edit" style="border: solid 1px; padding: 1% 3%; margin: 1% 0% 2% 0%">
 			
 			<select name="" id="select_poll"></select>
@@ -54,16 +48,22 @@
 
 		</div>
 		
-		<div id="div_create" style="border: solid 1px; padding: 1% 3%; margin: 1% 0% 2% 0%">
-			
 
-		</div>
+		<!-- ============ Seccion para crear nueva encuesta  ================= -->
+
+		<div id="div_create" style="border: solid 1px; padding: 1% 3%; margin: 1% 0% 2% 0%"></div>
+	
+		
+
+		<!-- ============ Seccion para ver la encuesta a editar  ================= -->
 
 		<div id="div_show_edit" style="border: solid 1px; padding: 1% 3%; margin: 1% 0% 2% 0%">
 			
 
 		</div>
 
+
+		<!-- ============ Seccion para mostrar la opcion de "crear nueva pregunta"  ================= -->
 
 		<div id="div_btn_new_quest" style="border: solid 1px; padding: 1% 3%; margin: 1% 0% 2% 0%">
 			
@@ -75,6 +75,8 @@
 
 		</div>
 
+		<!-- ============ Seccion para mostrar la opcion de guardar la encuesta  ================= -->
+
 		<div id="div_btn_send_poll" style="border: solid 1px; padding: 1% 3%; margin: 1% 0% 2% 0%">
 			
 			<form action="">
@@ -84,6 +86,8 @@
 
 
 		</div>
+
+		<!-- ============ Seccion para mostrar la url de la encuesta creada  ================= -->
 
 		<div id="div_show_url" style="border: solid 1px; padding: 1% 3%; margin: 1% 0% 2% 0%">
 			
