e6d481d7e4b9642f5460288330ef71758aa86d5d
Angel Torres <thekamitorres@gmail.com>
2015-08-21 20:03:28 -0500

    diferentes navbar

--- nav.php
+++ nav.php
@@ -16,63 +16,77 @@
 		src: local("Raleway SemiBold"), local("Raleway-SemiBold"), url("http://fonts.gstatic.com/s/raleway/v9/xkvoNo9fC8O2RDydKj12bwzyDMXhdD8sAj6OAJTFsBI.woff2") format("woff2"), url("http://fonts.gstatic.com/s/raleway/v9/xkvoNo9fC8O2RDydKj12b73hpw3pgy2gAi-Ip7WPMi0.woff") format("woff");
 	}
 
-	nav.primary{
-	display: table;
-	margin: 0 auto;
-	width: 100%;
-	/*background-color: red;*/
-	border: solid 1px gray;
-	 }
-	nav.primary ul,
-	nav.primary ul li {
-	margin: 0px;
-	}
+.div {
+box-shadow: 1px 1px 5px gray;
+}
 
-	nav.primary select {
-	display: none;
-	width:  100%;
-	height: 28px;
-	margin: 21px 0;
-	}
-
-	nav.primary ul li {
-	display: inline;
-	float: left;
-	position: relative;}
-
-	nav.primary ul li a {
-	display: inline-block;
-	line-height: 49px;
-	padding:  0 14px;
-	color: black;
-	text-transform: uppercase;
-	text-decoration: none;
-	font-weight: Raleway;
-	letter-spacing: 0.08em;
-	background: ##999999;
-
-	}
-
-	nav.primary ul li a:hover {
-	background: #d9e7e6;
-	cursor: pointer;
-
-	}
   </style>
 <body>
+
 	<!--Navigation-->
-<div class="band navigation">
-    <nav class=" primary">
+<div class=" band navigation">
+    <nav class=" primary div">
         <div class="sixteen columns">
             <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About Us</a></li>
-                <li><a href="#">Projects</a></li>
-                <li><a href="#">Contact Us</a></li>
+            	<li><a  class="navbar-link" href="#"></a></li>
+            	<li><a  class="navbar-link" href="#"></a></li>
+            	<li><a href="#"></a></li>
+                <li><a  class="navbar-link" href="#">Home</a></li>
+                <li><a  class="navbar-link" href="#">About Us</a></li>
+                <li><a  class="navbar-link" href="#">Projects</a></li>
+                <li><a  class="navbar-link" href="#">Contact Us</a></li>
             </ul>
         </div>
     </nav>
 </div>
+
+<!--Navigation-->
+<div class=" band navigation">
+    <nav class=" primary">
+        <div class="sixteen columns">
+            <ul>
+            	<li><a  class="navbar-link" href="#"></a></li>
+            	<li><a  class="navbar-link" href="#"></a></li>
+            	<li><a href="#"></a></li>
+                <li><a  class="navbar-link" href="#">Home</a></li>
+                <li><a  class="navbar-link" href="#">About Us</a></li>
+                <li><a  class="navbar-link" href="#">Projects</a></li>
+                <li><a  class="navbar-link" href="#">Contact Us</a></li>
+            </ul>
+        </div>
+    </nav>
+</div>
+
+	<!--Navigation-->
+<div class=" band navigation">
+    <nav class="container primary div">
+        <div class="sixteen columns">
+            <ul>
+                <li><a  class="navbar-link" href="#">Home</a></li>
+                <li><a  class="navbar-link" href="#">About Us</a></li>
+                <li><a  class="navbar-link" href="#">Projects</a></li>
+                <li><a  class="navbar-link" href="#">Contact Us</a></li>
+            </ul>
+        </div>
+    </nav>
+</div>
+
+<!--Navigation-->
+<div class=" band navigation">
+    <nav class="container primary">
+        <div class="sixteen columns">
+            <ul>
+                <li><a  class="navbar-link" href="#">Home</a></li>
+                <li><a  class="navbar-link" href="#">About Us</a></li>
+                <li><a  class="navbar-link" href="#">Projects</a></li>
+                <li><a  class="navbar-link" href="#">Contact Us</a></li>
+            </ul>
+        </div>
+    </nav>
+</div>
+
+
+
 
 <div class="container">
 	
